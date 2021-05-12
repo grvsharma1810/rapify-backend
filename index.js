7d8306809d8a987300990f3d86e9e00b200f63b0
grvsharma1810 <gs8280601@gmail.com>
2021-05-12 12:44:14 +0530

    deploy: environment varriable setup for DB

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const bodyParser = require('body-parser')
 const mongoose = require("mongoose")
 const cors = require("cors")
 const morgan = require('morgan')
-const fs = require('fs') 
-const {User} = require('./models/user.model')
-const {Video} = require('./models/video.model')
+const fs = require('fs')
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { User } = require('./models/user.model')
+const { Video } = require('./models/video.model')
 
 const app = express();
 
-app.use(morgan('common',{
-  stream: fs.createWriteStream('./access.log',{flags:'a'})
+app.use(morgan('common', {
+  stream: fs.createWriteStream('./access.log', { flags: 'a' })
 }));
 app.use(morgan('dev'))
 
@@ -23,30 +26,30 @@ const usersRoutes = require("./routes/users.routes.js")
 
 initializeDBConnection();
 
-app.get("/", (req,res) => {  
+app.get("/", (req, res) => {
   res.send("API for Rapify")
 })
 
-app.use("/login",async(req,res) => {
-  try{
+app.use("/login", async (req, res) => {
+  try {
     const userCredentials = {
-      email : req.body.email,
-      password : req.body.password
+      email: req.body.email,
+      password: req.body.password
     }
-    console.log({userCredentials})
+    console.log({ userCredentials })
     const user = await User.findOne(userCredentials);
-    console.log({user})
-    if(user){
-      res.status(200).json({success: true, data : {user}});
-    } else{
-      res.status(401).json({success:false,message : "User Credentials are invalid"});
+    console.log({ user })
+    if (user) {
+      res.status(200).json({ success: true, data: { user } });
+    } else {
+      res.status(401).json({ success: false, message: "User Credentials are invalid" });
     }
   } catch (err) {
     res.status(500).json({ success: false, message: "unable to login user", errorMessage: err.message })
   }
 })
 
-app.use("/videos",async (req,res) => {
+app.use("/videos", async (req, res) => {
   let videos = await Video.find({}).populate("user")
   console.log(videos);
   videos = videos.map(video => {
@@ -56,7 +59,7 @@ app.use("/videos",async (req,res) => {
     video.user.videos = undefined;
     return video;
   })
-  res.status(200).send({success:true,data : {videos}})
+  res.status(200).send({ success: true, data: { videos } })
 })
 
 app.use("/users", usersRoutes);
@@ -66,7 +69,7 @@ app.use("/users", usersRoutes);
  * Note: DO not MOVE. This should be the last route
  */
 app.use((req, res) => {
-  res.status(404).json({ success: false, message: "route not found on server, please check"})
+  res.status(404).json({ success: false, message: "route not found on server, please check" })
 })
 
 
@@ -76,10 +79,10 @@ app.use((req, res) => {
  */
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ success: false, message: "error occured, see the errMessage key for more details", errorMessage: err.message})
+  res.status(500).json({ success: false, message: "error occured, see the errMessage key for more details", errorMessage: err.message })
 })
 
-const PORT= process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Server Started On Port : ', PORT);
 });
